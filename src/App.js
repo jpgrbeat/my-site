4e1272a8b411e8f63381f438e24c93692da04173
Josh <joshua.ross.miller@gmail.com>
2019-03-05 16:38:36 -0500

    first animations

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import Home from './home/main'
 import Header from './site/header'
 import Footer from './site/footer'
-import imageSplitter from './img-split/imageSplitter'
+import ImageSplitter from './img-split/imageSplitter'
 class SvgBackground extends React.Component {
   render() {
     return (
@@ -13,25 +13,25 @@ class SvgBackground extends React.Component {
             height="100%"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink">
-   
-   <pattern id="pattern-cubes" x="0" y="126" patternUnits="userSpaceOnUse" width="126" height="200" viewBox="0 0 10 16"> 
-   
+
+   <pattern id="pattern-cubes" x="0" y="126" patternUnits="userSpaceOnUse" width="126" height="200" viewBox="0 0 10 16">
+
      <g id="cube">
        <path fill='#7bd4d6' className="left-shade" d="M0 0l5 3v5l-5 -3z" />
        <path fill='#8b8897' d="M10 0l-5 3v5l5 -3" />
      </g>
      <use x="5" y="8" xlinkHref="#cube" />
      <use x="-5" y="8" xlinkHref="#cube" />
-   
+
    </pattern>
- 
+
    <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-cubes)" />
-  
+
 </svg>
-      
 
 
-      
+
+
     );
   }
 }
@@ -45,6 +45,7 @@ class App extends Component {
       <div className="App hg" style={{background: dataUri}}>
       <Header/>
         <Home/>
+        <ImageSplitter/>
         <Footer/>
       </div>
     );
