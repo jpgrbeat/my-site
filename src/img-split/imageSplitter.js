f934b699d5d9cb8b5591de28bdb23ee223a2b835
Josh <joshua.ross.miller@gmail.com>
2019-03-07 15:57:25 -0500

    fixed imgs

--- src/img-split/imageSplitter.js
+++ src/img-split/imageSplitter.js
@@ -1,19 +1,25 @@
 import React,{Component} from 'react'
-
+import bl from '../assets/bottom-left-me.png'
+import br from '../assets/bottom-right-me.png'
+import tl from '../assets/left-top-me.png'
+import tr from '../assets/right-top-me.png'
 
 
 class ImageSplitter extends Component{
+  componentDidMount(){
+    console.log('yes')
+  }
     render(){
         return(
             <div className='is'>
               <img
-              class='tr' src='https://lh3.googleusercontent.com/XuiMiIWO2jcQgwxzBNmmPiRpLjGNgcGWeUnu02M4b45aDSEAojf9H5JdoPR2vGWWXdaMrRm44CPc-oXIOeB8PTm4AG4EfHgdyRT6DqZKHK1GF7Um4I8A5UjYyWObf3IpnxUMdWYarIqqjuwI8jYZGsjRqAhRCt14ROGu2RCGyEmPmgvgNvr3Owc9fAScUr81IDLKzw_twKgNpF5oJmVP6vQpIHtrVa-McPLeu3PCyFDVeKvpY4v20D8tPii89FaTbYZ4HfSeAP1_f2ne-v06pkbJNavN_pdnu3lb8IpKwjO_afwO-VVMjClcMiNtlI_bA0jLTZqb9MxFSVE5DAhhY7O7gx8vuX0z6HRwO-22gtJTGyi5CyzwDi_mgaGi1zjK-xyqdeuIsRRVz1IOxih2aP0Otj3ZdAO48KF6uGjLc5HW1hNo16p7Nw8k2vPo795fCPp-FG7scoyD-YICfD-MwHWI114C7tigiN00a0JyzB7sVm18F8fJSTQFeIUKXI5qlBluLCBd3lv1Nr5gSnWqIZ51PinN4YKsRnrSCch2Z_GN8QIGSdxg2SYc930RQ9xvljpOyjZ1deJSyXUlwz4TIHF3HO-w8yb--4dJzEUGla3Zx98LgLuLof5Yjfk8wLJ5ktJBQrsQjkNe6nZXRU6uiNMWIj0UcZU=w1138-h1422-no'/>
+              className='tl' src={tr}/>
               <img
-              className='tl' src='https://lh3.googleusercontent.com/cjEdU8AtF8ukkZ3N6LBXR9CYIqkE1CbSHZ1o_quz44VS-7BdAI9zVb0TFbOTa1cY9_ZFBRzTMuqgdeM4DBf8OLYhPbcM6npJw88pqO3OWn_LTuMwSbKxam2AiQ5v8rottSTNJfgJlOSmR2NPQm-X9Lup71T7MKHt8Ori9qd1mOA-tni6yp465yh9mQr0-ammk0QcjK4mCD_fD5hhn9p6bw4a5SfulMrZ6Ar2XMyOZghlNwJrjL1M8blaymWAfYI3mSTvK-arw-bGoo48nm66K_FpOkRNy52ovQ9gkojo-h59UEGItJJrhIX5oVjZ3HrzDGAMFLwCMTFD0HfTvBNfkkI0NH0BF0DLiW4Fb0NgSeOrnB06LdDwyBE8IpmiD6Ux--cLJ8FGSGzkWm-64ovLGHFhaeNbO_LehdKcnqJxpsTfuPeMoHNEl66VQWM_PMABvme4JpC0TV--NePjMumEds9OobbC9IJPnuJVn1nWeE4N1E7cmX8D5VkoWs6r5zeSC5kyHlPdWHN7KG-1TbpkmGSF7MKTOdkaKue4FynWvKdlQdLGlW3-NYyXGL-ZMm5-1i712f-FwsFxH16jyUEg-VGB48nEz4XLZpmGpLq6ehkfFlljhMaWZrA2ttKyMbjgvorBZu-nEMvAG8c5F_hGmj41noB97CQ=w1138-h1422-no'/>
+              className='tr' src={tl}/>
               <img
-              className='br' src='https://lh3.googleusercontent.com/1jlt-bT69xHythYCAeC4MbIXd5_XoKEnvaNAJmkKH5lEGS4R8r5jb-U4JQD9Q6vHtgaN4FQXbnU04JYegj4SgtpbB8GQ1v4p5OlQ75kJE-FZzuyLsXSdD5p8vTgwgZQ7PlVZImpbD9ekUBrSwyqBRVDTmw__uz_AnQSFzCXdoFMmIcTnUpuw4_rgDQV__d51o8UJpiMog4mM-x3dk9YaY4qLpSeyAT2rMe7REBWRxMMLm3Xs_6RoXTrwk0i-4tnI-Wi3_0fz89kl0rn4NAxrm8b-8M3NDU3TIpNKD0_cLO-aJsCD_Ar4oI4hZbEXvNd_TfCV6qvyHemA6C66XY7JxLYdAIcLZZVCB5L-MTYNpWrK0_m_sVJkYZXX9-ikk4KQi2RDGBc8uSp_8qT2U7q2VeqEop5z7d0kYMqBQOSJ8e3ffgIoLE8_2iXzbkRooN_Qi9apCndfpw3Wd-im0oUeGmHgNW0hgs8WYs2v7eWcHYSlyZUQX6frGUyZtK2baNRdzDd1ShfyrL4LYeM4y0PfcxpmbtTsB3eeMQxdbvDOjnhsUKL--P7Czk5Vj_IG6WpMekfIBR43itQlWo8DIbd4zZo3JFDRaHuSWklKDdWZiYWYt7y-SzZuxg0PIbBLwG5kOpyh0oPii5OFihWX93SN9yY6KjHTf6I=w1138-h1422-no'/>
+              className='bl' src={br}/>
               <img
-              className='bl' src='https://lh3.googleusercontent.com/mtkdpfcMsN3kK3AmDGxJggoJqFooGQ1G4VJOqFz-EtGeImcdlMIo_4tmBWFjybpdRoDKfvp2Klxta9T5p1H68m4AXh_AiIb9L1fQuiOkROTr11PQ-1HVNv_ayWzYB-wkWJJtRYKoMQ2zf7oj8QxVj18FGXKl10wsU3dgOlKmPcr2cIA_nv4Tg8tWfl00FynMacEcH0Hf2m5FX5_Fn5myEbXrDlPc40k9NLknyhiGl4bj4EGsC1nZc_szXZgq0z0EXg64ij7gjaopuKvfOsSXTIfS7-9UYzLMAk9JJ01E6swqSsfJidoCB67tRaWjrq3wrvrb2zsd7KJgC6EePIffO4Hi66qLgbPkDtIzQ5j4BSQCmnPeEtUAHCa-eriodZ3R_QiugId_WajJKp-JOXoUh3Z-afn61C66pFhWst9KIeBb9r8VJiCvxfW1gZWjHjIfT9CALcr9HnYpZyGrr1_E_Ggw3gh_u-cnz7q_Eo-lucBZK0GrdNOtBe4PF-3P_0kw_B0YKBEaKnx3JDpVrYf9Zg7FNblzKTImCTXxo2zn0xmJG7XheQPGmVCefjKbgfiIaWgf-OkINtXyBhyPLPl5nHqtxP70i88hrVwPG6avswa2ZP2dGx7dbcOIb_24GCWYvA8CNcfsFNa7WIFZiBuIaLyBQWan9yI=w1138-h1422-no'/>
+              className='br' src={bl}/>
             </div>
         )
     }
